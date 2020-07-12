16952d5e85a6bda7ccf735ca6c74ca5835d32a06
bibinmac <bibinlouis666@gmail.com>
2020-07-12 12:17:10 +0530

    Add project files.

---
+++ Pages/Restaurants/List.cshtml.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class ListModel : PageModel
+    {
+        private readonly IConfiguration config;
+        private readonly IRestaurantData restaurantData;
+        private readonly ILogger<ListModel> logger;
+        public string Message { get; set; }
+        public IEnumerable<Restaurant> Restaurants { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string SearchTerm { get; set; }
+        public ILogger<ListModel> Logger { get; }
+
+        public ListModel(IConfiguration config, 
+                         IRestaurantData restaurantData,
+                         ILogger<ListModel> logger)
+        {
+            this.config = config;
+            this.restaurantData = restaurantData;
+            this.logger = logger;
+        }
+        public void OnGet()
+        {
+            logger.LogError("Executing List Model"); 
+            Message = config["Message"];
+            Restaurants = restaurantData.GetRestaurantsByName(SearchTerm);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using OdeToFood.Data;
+
+namespace OdeToFood
+{   
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //string assemblyName = typeof(OdeToFoodDbContext).Namespace;
+            //services.AddDbContextPool<OdeToFoodDbContext>(options =>
+            //options.UseSqlServer(Configuration.GetConnectionString("OdeToFoodDb"), optionsBuilder => optionsBuilder.MigrationsAssembly(assemblyName)));
+
+            services.AddDbContextPool<OdeToFoodDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("OdeToFoodDb")));
+
+            services.AddScoped<IRestaurantData, SqlRestaurantData>();
+            services.AddRazorPages();
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, 
+                              IHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.Use(SayHelloMiddleware);
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseNodeModules(); 
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+            
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+                endpoints.MapControllers();
+            });
+        }
+
+        private RequestDelegate SayHelloMiddleware(RequestDelegate next)
+        {
+            return async ctx =>
+            {
+                if (ctx.Request.Path.StartsWithSegments("/hello")) {
+                    await ctx.Response.WriteAsync("Hello, World");
+                }
+                else
+                {
+                    await next(ctx);
+                }
+            };
+        }
+    }
+}
